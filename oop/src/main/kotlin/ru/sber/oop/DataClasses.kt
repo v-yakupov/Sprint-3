97e790e20dd82882f321a05a971e2472b33870a9
v-yakupov <54433770+v-yakupov@users.noreply.github.com>
2021-09-13 18:16:51 +0300

    Equals fix

--- oop/src/main/kotlin/ru/sber/oop/DataClasses.kt
+++ oop/src/main/kotlin/ru/sber/oop/DataClasses.kt
@@ -3,8 +3,14 @@ package ru.sber.oop
 data class User(val name: String, val age: Long) {
     lateinit var city: String
 
-    fun equals(other: User): Boolean = equals(other as Any) &&
-                if (other::city.isInitialized && this::city.isInitialized) this.city == other.city else false
+    override fun equals(other: Any?): Boolean =
+        if (other is User)
+            other::city.isInitialized &&
+            this::city.isInitialized &&
+            other.name == this.name &&
+            other.age == this.age &&
+            other.city == this.city
+        else false
 }
 
 fun main() {
